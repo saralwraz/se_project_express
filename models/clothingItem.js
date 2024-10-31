a49f8ed4d5ceba5d608b56f02815c0ad94c13ec2
Sara Raz <saralwraz@gmail.com>
2024-10-31 14:24:22 +0200

    User + Clothing Schemas + adding Validator

---
+++ models/clothingItem.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const clothingSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  weather: {
+    type: String,
+    enum: ["hot", "warm", "cold"],
+    required: true,
+  },
+  imageURL: {
+    type: String,
+    required: [true, "This field is required."],
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  likes: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("clothing", clothingSchema);
